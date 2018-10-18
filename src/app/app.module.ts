81f886a61a68c0e2437075273e122c718152f3d3
werick silva <wericksilva-@hotmail.com>
2018-10-17 21:38:53 -0300

    correção # rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 import { FormsModule }   from '@angular/forms';
 import {ROUTES} from './app.routes';
 import { AppComponent } from './app.component';
@@ -18,6 +18,7 @@ import { AtividadeItemComponent } from './pacientes/paciente-detalhes/atividade-
 import { AvaliacoesComponent } from './pacientes/paciente-detalhes/avaliacoes/avaliacoes.component';
 import { CadastroPacientesComponent } from './pacientes/cadastro-pacientes/cadastro-pacientes.component';
 import { InputComponent } from './util/input/input.component';
+import {LocationStrategy, HashLocationStrategy} from '@angular/common'
 
 
 
@@ -42,9 +43,9 @@ import { InputComponent } from './util/input/input.component';
     BrowserModule,
     HttpModule,
     FormsModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
   ],
-  providers: [PacientesService],
+  providers: [PacientesService, {provide: LocationStrategy, useClass: HashLocationStrategy}, {provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
